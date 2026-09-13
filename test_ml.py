db1c042af9c8a9f7e64acdee71e028dca6653b7b
Avery Black <averyblack17@ymail.com>
2026-09-13 14:49:55 -0600

    added unit tests to test_ml

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,42 @@
 import pytest
-# TODO: add necessary import
+from train_model import data, cat_features, y_test, preds, model, X_test
+from ml.data import process_data
+from ml.model import compute_model_metrics, inference
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
+
+def test_process_data_shape():
     """
-    # add description for the first test
+    Tests if original data has the same number of rows as the processed data.
+
     """
-    # Your code here
-    pass
+    X,y, encoder, lb = process_data(
+        data,
+        categorical_features=cat_features,
+        label="salary",
+        training=True,
+    )
+    assert X.shape[0] == data.shape[0]
+    assert y.shape[0] == data.shape[0]
+    
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_compute_model_metrics():
     """
-    # add description for the second test
+    # Tests if the model metrics are all float values and are between 0.0 and 1.0.
     """
-    # Your code here
-    pass
+    p, r, fb = compute_model_metrics(y_test, preds)
+    assert isinstance(p, float)
+    assert isinstance(r, float)
+    assert isinstance(fb, float)
+    assert 0.0 <= p <= 1.0
+    assert 0.0 <= r <= 1.0
+    assert 0.0 <= fb <= 1.0
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_prediction():
     """
-    # add description for the third test
+    Tests that prediction function returns 0 or 1.
     """
-    # Your code here
-    pass
+    preds = inference(model, X_test)
+    assert all(pred in [0, 1] for pred in preds)
